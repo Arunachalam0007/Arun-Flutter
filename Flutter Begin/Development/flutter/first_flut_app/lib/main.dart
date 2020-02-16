440ca914c358bbf11e59b77690facf683410ca69
UiPathChennaiWorkshopTask <arunsha0007@gmail.com>
2020-02-16 22:45:09 +0530

    Stateful Widget, with setState method.

--- Flutter Begin/Development/flutter/first_flut_app/lib/main.dart
+++ Flutter Begin/Development/flutter/first_flut_app/lib/main.dart
@@ -6,7 +6,30 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  var questions = [
+    'Do u like Cricket ?',
+    'Do u like Football ?',
+    'Do u like Tennis ?'
+  ];
+
+  void answerQuestions() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex.toString() + ' ' + questions[questionIndex]);
+  }
+
   @override
   Widget build(BuildContext context) {
     // Build is a abstract method
@@ -18,7 +41,28 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Sha Demo App'),
         ),
-        body: Text('Welcome Back to Scaffold Default Flutter Template'),
+        // body: Text('Welcome Back to Scaffold Default Flutter Template'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Question 1'),
+              onPressed: () {
+                print("Yes....");
+              },
+            ),
+            RaisedButton(
+              child: Text('Question 2'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Question 3'),
+              onPressed: answerQuestions,
+            )
+          ],
+        ),
       ),
     );
   }
