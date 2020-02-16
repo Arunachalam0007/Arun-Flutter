11dd99e11d064624cf59d367341bbf4adc189e15
UiPathChennaiWorkshopTask <arunsha0007@gmail.com>
2020-02-16 20:45:23 +0530

    Scaffold Default app widget (Flutter)

--- Flutter Begin/Development/flutter/first_flut_app/lib/main.dart
+++ Flutter Begin/Development/flutter/first_flut_app/lib/main.dart
@@ -6,14 +6,20 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     // Build is a abstract method
     // Context -->It's a special type of object, It’s used to store the metadata info of app or widget,Flutter will call the whenever need to draw.
     // TODO: implement build
     // MaterialApp --> It's a Widget or Instance/object of the MaterialApp from Flutter Material.dart file
-    return MaterialApp(home: Text('Hello SHA!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sha Demo App'),
+        ),
+        body: Text('Welcome Back to Scaffold Default Flutter Template'),
+      ),
+    );
   }
-
 }
