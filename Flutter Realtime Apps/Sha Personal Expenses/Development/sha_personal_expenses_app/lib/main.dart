c74a7a0964dd62b32fa23c19c7b86eaf908233c6
UiPathChennaiWorkshopTask <arunsha0007@gmail.com>
2020-02-23 20:54:50 +0530

    Sha RealTime Flutter App (Sha Expenses Tracker)

---
+++ Flutter Realtime Apps/Sha Personal Expenses/Development/sha_personal_expenses_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sha Daily Expenses'),
+      ),
+      body: Center(
+        child: Text('Sha Real Time Application Begins'),
+      ),
+    );
+  }
+}
